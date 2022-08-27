38bc8419236c36e8e179dbc95008ec2a9e20caaf
JustFantick <68466135+JustFantick@users.noreply.github.com>
2022-08-27 20:56:06 +0300

    Adaptived header: added menu-burger if viewport < 900px

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,19 +2,83 @@ import React, { Component } from 'react'
 import Button from '../button/button.jsx'
 
 export default class Header extends Component {
+	constructor(props) {
+		super(props);
+		this.burgerClickHandler = this.burgerClickHandler.bind(this);
+	}
+
+	burgerClickHandler(e) {
+		let burger = e.target.closest('.menuBurger');
+		let anchors = document.querySelector('.anchorList');
+
+		burger.classList.toggle('active');
+		anchors.classList.toggle('active');
+
+		anchors.addEventListener('click', function () {
+			burger.classList.toggle('active');
+			anchors.classList.toggle('active');
+		}, { once: true });
+	}
+
+
 	render() {
+		const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+		let anchorList, rightSection;
+		console.log(vw);
+
+		if (vw < 900 && vw > 375) {
+			anchorList =
+				<ul className="anchorList">
+					<li className='anchorList__item'>Home</li>
+					<li className='anchorList__item'>Courses</li>
+					<li className='anchorList__item'>Deals</li>
+					<li className='anchorList__item'>Success</li>
+					<li className='anchorList__item'>About</li>
+					<li className='anchorList__item'>Register</li>
+				</ul>;
+			rightSection =
+				<div className='header__rightSection'>
+					<Button type='white' text='Register' />
+					<div className='menuBurger' onClick={this.burgerClickHandler}>
+						<span></span>
+					</div>
+				</div>;
+		} else if (vw <= 375) {
+			anchorList =
+				<ul className="anchorList">
+					<Button type='white' text='Register' />
+					<li className='anchorList__item'>Home</li>
+					<li className='anchorList__item'>Courses</li>
+					<li className='anchorList__item'>Deals</li>
+					<li className='anchorList__item'>Success</li>
+					<li className='anchorList__item'>About</li>
+					<li className='anchorList__item'>Register</li>
+				</ul>;
+			rightSection =
+				<div className='menuBurger' onClick={this.burgerClickHandler}>
+					<span></span>
+				</div>;
+		} else {
+			anchorList =
+				<ul className="anchorList">
+					<li className='anchorList__item'>Home</li>
+					<li className='anchorList__item'>Courses</li>
+					<li className='anchorList__item'>Deals</li>
+					<li className='anchorList__item'>Success</li>
+					<li className='anchorList__item'>About</li>
+					<li className='anchorList__item'>Register</li>
+				</ul>;
+			rightSection = <Button type='white' text='Register' />;
+		}
+
+
 		return (
 			<div className='header'>
 				<div className="header__title">Pro Edu</div>
-				<ul className="header__list">
-					<li className='header__list-item'>Home</li>
-					<li className='header__list-item'>Courses</li>
-					<li className='header__list-item'>Deals</li>
-					<li className='header__list-item'>Success</li>
-					<li className='header__list-item'>About</li>
-					<li className='header__list-item'>Register</li>
-				</ul>
-				<Button type='white' text='Register' />
+
+				{anchorList}
+
+				{rightSection}
 			</div>
 		)
 	}
