225bd156ab964209a6195ed2fa3d2cd08efcf7a9
JustFantick <68466135+JustFantick@users.noreply.github.com>
2022-09-25 21:30:15 +0300

    Adaptived "institute" block

--- src/components/institute/institute.jsx
+++ src/components/institute/institute.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
+import Button from '../button/button.jsx';
+
 class Institute extends Component {
 	render() {
 		return (
 			<section className='institute'>
-				<div className="institute__image"></div>
+				<div className="institute__image">
+					<img alt='institute.jpg'
+						src={
+							window.innerWidth < 880 ?
+								require('../../img/institute_mb.jpg')
+								: require('../../img/institute_pc.jpg')
+						}
+					/>
+				</div>
 				<div className="institute__content inst-content">
 					<div className="inst-content__title">Explore The elearning Institute</div>
 					<div className="inst-content__text">
@@ -12,18 +22,19 @@ class Institute extends Component {
 						<p>Anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined.</p>
 					</div>
 					<div className="inst-content__numbers">
-						<div className='institute-big-number'>
+						<div className='instituteBigNumber'>
 							<h4>3.2K+</h4>
 							<p>Online Course</p>
 						</div>
-						<div className='institute-big-number'>
+						<div className='instituteBigNumber'>
 							<h4>600+</h4>
 							<p>Expert member</p>
-						</div><div className='institute-big-number'>
+						</div><div className='instituteBigNumber'>
 							<h4>1K+</h4>
 							<p>Rating & Review</p>
 						</div>
 					</div>
+					<Button type='blue' text='Read more' />
 
 				</div>
 
