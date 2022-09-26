486ee4aef382742b91cbdccee5e6fcb45a33c74b
JustFantick <68466135+JustFantick@users.noreply.github.com>
2022-09-26 23:04:55 +0300

    Added base ver of "text add animation"

--- src/components/institute/institute.jsx
+++ src/components/institute/institute.jsx
@@ -3,7 +3,67 @@ import React, { Component } from 'react';
 import Button from '../button/button.jsx';
 
 class Institute extends Component {
+	constructor(props) {
+		super(props)
+
+		this.handleStringChange = this.handleStringChange.bind(this);
+		this.state = {}
+	}
+
+	handleStringChange(e) {
+		const textToAdd = " don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure.";
+
+		let target = e.target;
+		let i = 0, length = textToAdd.length;
+
+		if (target.textContent.length > 168) {
+			let i = target.textContent.length;
+
+			let id = setInterval(() => {
+
+				if (i > 165) {
+					target.textContent = target.textContent.substring(0, i - 1);
+					i--;
+				} else {
+					target.textContent = target.textContent.concat("...");
+					clearInterval(id);
+				}
+			}, 10);
+		} else {
+			e.target.textContent = e.target.textContent.substring(0, e.target.textContent.length - 3);
+
+			let id = setInterval(() => {
+				if (i < length) {
+					e.target.textContent = target.textContent.concat(textToAdd[i]);
+					i++;
+				} else {
+					clearInterval(id);
+				}
+			}, 10);
+		}
+	}
+
 	render() {
+
+		// const textToAdd = "don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure.";
+
+		const textPc = <div className="inst-content__text"
+			onClick={this.handleStringChange}>
+			There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which...
+			{/* <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which...</p> */}
+			{/* <p>Anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined.</p> */}
+		</div>;
+
+		const textMb = <div className="inst-content__text">
+			There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which
+			<div className='inst-content__mobile-ver' onClick={this.handleStringChange}>
+				<div>
+					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure.</p>
+					<p>Anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined.</p>
+				</div>
+			</div>
+		</div>;
+
 		return (
 			<section className='institute'>
 				<div className="institute__image">
@@ -17,10 +77,7 @@ class Institute extends Component {
 				</div>
 				<div className="institute__content inst-content">
 					<div className="inst-content__title">Explore The elearning Institute</div>
-					<div className="inst-content__text">
-						<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure.</p>
-						<p>Anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined.</p>
-					</div>
+					{window.innerWidth < 880 ? textPc : textPc}
 					<div className="inst-content__numbers">
 						<div className='instituteBigNumber'>
 							<h4>3.2K+</h4>
