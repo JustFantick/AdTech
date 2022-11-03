39b8bdf30469f6aa0dc8ec2d408a9bfb36ff2c7b
JustFantick <68466135+JustFantick@users.noreply.github.com>
2022-11-03 21:38:28 +0200

    Done star-rating + adaptived feedback`s slider

--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -7,7 +7,220 @@ import 'swiper/less';
 import './feedback.less';
 
 export default class Feedback extends Component {
+	componentDidMount() {
+
+	}
+
 	render() {
+		let viewOverBreakpoint = <div className="feedback__slider">
+			<Swiper
+				modules={[Navigation]}
+				spaceBetween={50}
+				slidesPerView={2}
+				speed={700}
+				navigation={
+					{
+						prevEl: '.prev',
+						nextEl: '.next',
+					}
+				}
+				onSwiper={(swiper) => console.log(swiper)}
+				onSlideChange={() => console.log('slide change')}
+
+			>
+				<SwiperSlide>
+					<div className='slide-top'>
+						<div className='slider-top-quoter'>
+							<img src={require('../../img/quoter.svg')} alt="" />
+						</div>
+						<div className='slider-top-stars rating'>
+							<div className="rating__active"></div>
+							<div className="rating__items">
+								<input type="radio" className='rating__item' value='1' name='rating' />
+								<input type="radio" className='rating__item' value='2' name='rating' />
+								<input type="radio" className='rating__item' value='3' name='rating' />
+								<input type="radio" className='rating__item' value='4' name='rating' />
+								<input type="radio" className='rating__item' value='5' name='rating' />
+							</div>
+
+						</div>
+					</div>
+					<div className='slide-text'>
+						There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem.
+					</div>
+					<div className='slide-author'>
+						<div className='slide-author-image'>
+							<img src={require('../../img/feedback_1.jpg')} alt="Image_lost" />
+						</div>
+						<div className='slide-author-name'>
+							<h6>Awlad Hossain</h6>
+							<p>UI Designer</p>
+						</div>
+					</div>
+				</SwiperSlide>
+
+				<SwiperSlide>
+					<div className='slide-top'>
+						<div className='slider-top-quoter'>
+							<img src={require('../../img/quoter.svg')} alt="" />
+						</div>
+						<div className='slider-top-stars rating'>
+							<div className="rating__active"></div>
+							<div className="rating__items">
+								<input type="radio" className='rating__item' value='1' name='rating' />
+								<input type="radio" className='rating__item' value='2' name='rating' />
+								<input type="radio" className='rating__item' value='3' name='rating' />
+								<input type="radio" className='rating__item' value='4' name='rating' />
+								<input type="radio" className='rating__item' value='5' name='rating' />
+							</div>
+
+						</div>
+					</div>
+					<div className='slide-text'>
+						There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem.
+					</div>
+					<div className='slide-author'>
+						<div className='slide-author-image'>
+							<img src={require('../../img/feedback_2.jpg')} alt="Image_lost" />
+						</div>
+						<div className='slide-author-name'>
+							<h6>Shanta Akter</h6>
+							<p>Graphic Designer</p>
+						</div>
+					</div>
+				</SwiperSlide>
+
+				<SwiperSlide>
+					<div className='slide-top'>
+						<div className='slider-top-quoter'>
+							<img src={require('../../img/quoter.svg')} alt="" />
+						</div>
+						<div className='slider-top-stars rating'>
+							<div className="rating__active"></div>
+							<div className="rating__items">
+								<input type="radio" className='rating__item' value='1' name='rating' />
+								<input type="radio" className='rating__item' value='2' name='rating' />
+								<input type="radio" className='rating__item' value='3' name='rating' />
+								<input type="radio" className='rating__item' value='4' name='rating' />
+								<input type="radio" className='rating__item' value='5' name='rating' />
+							</div>
+
+						</div>
+					</div>
+					<div className='slide-text'>
+						There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem.
+					</div>
+					<div className='slide-author'>
+						<div className='slide-author-image'>
+							<img src={require('../../img/feedback_1.jpg')} alt="Image_lost" />
+						</div>
+						<div className='slide-author-name'>
+							<h6>Awlad Hossain</h6>
+							<p>UI Designer</p>
+						</div>
+					</div>
+				</SwiperSlide>
+
+				<SwiperSlide>
+					<div className='slide-top'>
+						<div className='slider-top-quoter'>
+							<img src={require('../../img/quoter.svg')} alt="" />
+						</div>
+						<div className='slider-top-stars rating'>
+							<div className="rating__active"></div>
+							<div className="rating__items">
+								<input type="radio" className='rating__item' value='1' name='rating' />
+								<input type="radio" className='rating__item' value='2' name='rating' />
+								<input type="radio" className='rating__item' value='3' name='rating' />
+								<input type="radio" className='rating__item' value='4' name='rating' />
+								<input type="radio" className='rating__item' value='5' name='rating' />
+							</div>
+
+						</div>
+					</div>
+					<div className='slide-text'>
+						There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem.
+					</div>
+					<div className='slide-author'>
+						<div className='slide-author-image'>
+							<img src={require('../../img/feedback_2.jpg')} alt="Image_lost" />
+						</div>
+						<div className='slide-author-name'>
+							<h6>Shanta Akter</h6>
+							<p>Graphic Designer</p>
+						</div>
+					</div>
+				</SwiperSlide>
+
+				<div className='icon-arrow prev inside-circle'></div>
+				<div className='icon-arrow next inside-circle'></div>
+			</Swiper>
+		</div>;
+
+		let viewUnderBreakpoint =
+			<div className="feedback__slider">
+				<div className='slide'>
+					<div className='slide-top'>
+						<div className='slider-top-quoter'>
+							<img src={require('../../img/quoter.svg')} alt="" />
+						</div>
+						<div className='slider-top-stars rating'>
+							<div className="rating__active"></div>
+							<div className="rating__items">
+								<input type="radio" className='rating__item' value='1' name='rating' />
+								<input type="radio" className='rating__item' value='2' name='rating' />
+								<input type="radio" className='rating__item' value='3' name='rating' />
+								<input type="radio" className='rating__item' value='4' name='rating' />
+								<input type="radio" className='rating__item' value='5' name='rating' />
+							</div>
+
+						</div>
+					</div>
+					<div className='slide-text'>
+						There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem.
+					</div>
+					<div className='slide-author'>
+						<div className='slide-author-image'>
+							<img src={require('../../img/feedback_1.jpg')} alt="Image_lost" />
+						</div>
+						<div className='slide-author-name'>
+							<h6>Awlad Hossain</h6>
+							<p>UI Designer</p>
+						</div>
+					</div>
+				</div>
+				<div className='slide'>
+					<div className='slide-top'>
+						<div className='slider-top-quoter'>
+							<img src={require('../../img/quoter.svg')} alt="" />
+						</div>
+						<div className='slider-top-stars rating'>
+							<div className="rating__active"></div>
+							<div className="rating__items">
+								<input type="radio" className='rating__item' value='1' name='rating' />
+								<input type="radio" className='rating__item' value='2' name='rating' />
+								<input type="radio" className='rating__item' value='3' name='rating' />
+								<input type="radio" className='rating__item' value='4' name='rating' />
+								<input type="radio" className='rating__item' value='5' name='rating' />
+							</div>
+
+						</div>
+					</div>
+					<div className='slide-text'>
+						There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem.
+					</div>
+					<div className='slide-author'>
+						<div className='slide-author-image'>
+							<img src={require('../../img/feedback_2.jpg')} alt="Image_lost" />
+						</div>
+						<div className='slide-author-name'>
+							<h6>Shanta Akter</h6>
+							<p>Graphic Designer</p>
+						</div>
+					</div>
+				</div>
+			</div>;
+
 		return (
 			<section className='feedback'>
 				<div className="feedback__text">
@@ -17,68 +230,10 @@ export default class Feedback extends Component {
 					</p>
 				</div>
 
-				<div className="feedback__slider">
-					<Swiper
-						modules={[Navigation]}
-						spaceBetween={50}
-						slidesPerView={2}
-						speed={700}
-						navigation={
-							{
-								prevEl: '.prev',
-								nextEl: '.next',
-							}
-						}
-						onSwiper={(swiper) => console.log(swiper)}
-						onSlideChange={() => console.log('slide change')}
-
-					>
-						<SwiperSlide>
-							<div className='slide-top'>
-								<div className='slider-top-quoter'>
-									<img src={require('../../img/quoter.svg')} alt="" />
-								</div>
-								<div className='slider-top-stars'></div>
-							</div>
-							<div className='slide-text'>
-								There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem.
-							</div>
-							<div className='slide-author'>
-								<div className='slide-author-image'>
-									<img src={require('../../img/feedback_1.jpg')} alt="Image_lost" />
-								</div>
-								<div className='slide-author-name'>
-									<h6>Awlad Hossain</h6>
-									<p>UI Designer</p>
-								</div>
-							</div>
-						</SwiperSlide>
-
-						<SwiperSlide>
-							<div className='slide-top'>
-								<div className='slider-top-quoter'>
-									<img src={require('../../img/quoter.svg')} alt="" />
-								</div>
-								<div className='slider-top-stars'></div>
-							</div>
-							<div className='slide-text'>
-								There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem.
-							</div>
-							<div className='slide-author'>
-								<div className='slide-author-image'>
-									<img src={require('../../img/feedback_2.jpg')} alt="Image_lost" />
-								</div>
-								<div className='slide-author-name'>
-									<h6>Shanta Akter</h6>
-									<p>Graphic Designer</p>
-								</div>
-							</div>
-						</SwiperSlide>
-
-						<div className='icon-arrow prev inside-circle'></div>
-						<div className='icon-arrow next inside-circle'></div>
-					</Swiper>
-				</div>
+				{
+					window.innerWidth > 1000 ?
+						viewOverBreakpoint : viewUnderBreakpoint
+				}
 
 			</section>
 		)
